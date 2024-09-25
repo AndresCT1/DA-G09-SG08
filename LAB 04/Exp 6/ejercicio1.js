1493ce8cfcb1f8f6c4774183b8e557cb8b158525
Ruby-Mollo <132023303+Ruby-Mollo@users.noreply.github.com>
2024-09-25 00:23:35 -0500

    Delete LAB 04/Exp 6 directory

--- LAB 04/Exp 6/ejercicio1.js
+++
@@ -1,5 +0,0 @@
-// Mostrar un mensaje de alerta
-window.alert('Este es un mensaje de alerta');
-
-// Mostrar el ancho de la ventana del navegador
-console.log(window.innerWidth);
--- LAB 04/Exp 6/ejercicio2.js
+++
@@ -1,7 +0,0 @@
-// Generar un número aleatorio entre 0 y 1
-let numeroAleatorio = Math.random();
-console.log(numeroAleatorio);
-
-// Redondear un número hacia abajo
-let redondeo = Math.floor(4.9);
-console.log(redondeo); // Imprime: 4
